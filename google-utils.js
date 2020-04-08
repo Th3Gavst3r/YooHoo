23d7c5361f0440467c41dd1f982ab4917aacdbc8
Th3Gavst3r <th3gavst3r@gmail.com>
2020-04-07 22:10:22 -0400

    Implement google OAuth

---
+++ google-utils.js
@@ -0,0 +1,50 @@
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const PORT = process.env.PORT;
+
+const { google } = require('googleapis');
+const googleConfig = {
+  clientId: CLIENT_ID,
+  clientSecret: CLIENT_SECRET,
+  redirect: `http://127.0.0.1:${PORT}/callback`, // this must match your google api settings
+};
+
+const auth = createConnection();
+
+/**
+ * Create the google auth object which gives us access to talk to google's apis.
+ */
+function createConnection() {
+  return new google.auth.OAuth2(
+    googleConfig.clientId,
+    googleConfig.clientSecret,
+    googleConfig.redirect
+  );
+}
+
+/**
+ * This scope tells google what information we want to request.
+ */
+const defaultScope = ['https://www.googleapis.com/auth/youtube'];
+
+/**
+ * Get a url which will open the google sign-in page and request access to the scope provided (such as calendar events).
+ */
+function getConnectionUrl() {
+  return auth.generateAuthUrl({
+    access_type: 'offline',
+    prompt: 'consent', // access type and approval prompt will force a new refresh token to be made each time signs in
+    scope: defaultScope,
+  });
+}
+
+/**
+ * Exchange authCode for access and refresh tokens
+ */
+async function setTokens(authCode) {
+  const data = await auth.getToken(authCode);
+  const tokens = data.tokens;
+  auth.setCredentials(tokens);
+}
+
+module.exports = { getConnectionUrl, setTokens };
--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 require('dotenv').config();
+const PORT = process.env.PORT;
 const TOKEN = process.env.TOKEN;
 
 const Discord = require('discord.js');
+const express = require('express');
+const gsUtils = require('./google-utils');
+
 const client = new Discord.Client();
+const app = express();
+
 const urlRegex = /((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?/g;
+
+/**
+ * Google OAuth landing pages
+ */
+app.get('/', (req, res) => res.redirect(gsUtils.getConnectionUrl()));
+app.get('/callback', (req, res) => {
+  gsUtils.setTokens(req.query.code);
+  res.send('hello user');
+});
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
