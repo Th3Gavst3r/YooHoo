252212edb7141732ad6a02c9fe32ec1e7dcd9d81
Th3Gavst3r <th3gavst3r@gmail.com>
2020-07-01 23:40:20 -0400

    Create privacy policy page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 require('./discord-handler');
 const express = require('express');
+const favicon = require('serve-favicon');
+const path = require('path');
 const { port } = require('./config');
+const privacy = require('./routes/privacy');
 const register = require('./routes/register');
 
 const app = express();
 
+app.use(favicon(path.join(__dirname, 'views', 'img', 'favicon.ico')));
 app.use('/register', register);
+app.use('/privacy', privacy);
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
